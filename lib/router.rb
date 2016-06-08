99c449a822cd2c6073a877cb27a6d22579468094
jordvnkm <minatogawaj@gmail.com>
2016-06-07 17:01:06 -0700

    finished phase 1-4

--- lib/router.rb
+++ lib/router.rb
@@ -2,31 +2,51 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    match1 = /#{@pattern}/.match(req.path)
+
+    if !match1.nil? && match1[0] == req.path
+      if @http_method == req.request_method.downcase.to_sym
+        return true
+      end
+    end
+    false
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    if matches?(req)
+      @controller = @controller_class.new(req, res, {})
+      @controller.invoke_action(http_method)
+    end
   end
+
 end
 
 class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes.push(Route.new(pattern, method, controller_class, action_name))
   end
 
   # evaluate the proc in the context of the instance
   # for syntactic sugar :)
   def draw(&proc)
+    
   end
 
   # make each of these methods that
--- lib/session.rb
+++ lib/session.rb
@@ -4,16 +4,29 @@ class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookie = req.cookies['_rails_lite_app']
+    if cookie
+      @my_cookie = JSON.parse(cookie)
+    else
+      @my_cookie = {}
+    end
   end
 
   def [](key)
+    @my_cookie[key]
   end
 
   def []=(key, val)
+    @my_cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie(
+      "_rails_lite_app",
+      path: "/",
+      value: @my_cookie.to_json
+    )
   end
 end
